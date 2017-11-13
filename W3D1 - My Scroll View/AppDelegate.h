85254a0c045cec06c3d7c3f8dac3888bf81b2a04
brokeandfurious <forumbomb@gmail.com>
2017-11-13 11:22:27 -0500

    Initial Commit

---
+++ W3D1 - My Scroll View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W3D1 - My Scroll View
+//
+//  Created by Murat Ekrem Kolcalar on 11/13/17.
+//  Copyright © 2017 murtilicious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
